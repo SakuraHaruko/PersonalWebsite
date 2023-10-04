e1a061c0a588548a3016d40a895866178f23cf95
buguwu <buguwu.official@outlook.com>
2023-10-04 21:33:45 +0800

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+      <Head>
+        <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
+        <meta name="keywords" content="buguwu bug homepage" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={process.env.NEXT_PUBLIC_TITLE} />
+        <meta property="og:description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
+        <meta property="og:image" content="https://example.com/your-image.jpg" />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_BASEURL_PROD} />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,67 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const index = () => {
+    return (
+        <div className="container">
+          <h1>Helloo</h1>
+          <p>This is a demo. :D</p>
+
+          {/* Social Media Buttons */}
+        <div className="flex justify-center space-x-4 mt-4">
+          <SocialMediaButton icon={faTwitter} tooltip="Twitter" link="https://x.com/buguwuofficial" />
+          <SocialMediaButton icon={faGithub} tooltip="GitHub" link="https://github.com/buguwu" />
+          <SocialMediaButton icon={faDiscord} tooltip="Discord" link="#" />
+          <SocialMediaButton icon={faEnvelope} tooltip="Email" link="mailto:i@qwq.lgbt" />
+        </div>
+        </div>
+
+        
+    );
+  };
+  
+// Social Media Button Component
+const SocialMediaButton = ({ icon, tooltip, link }: { icon: any, tooltip: string, link: string }) => {
+  return (              
+    <a href={link} target="_blank" rel="noopener noreferrer" className="relative group">
+      <div className="w-10 h-10 flex items-center justify-center bg-blue-500 rounded-full text-white transition-transform transform group-hover:scale-110">
+        <FontAwesomeIcon icon={icon} className="text-xl" />
+      </div>
+      {/* 使用 :hover 伪类来触发按钮和提示的显示和隐藏 */}
+      <div className="tooltip-container relative group-hover:visible invisible bg-transparent text-transparent text-xs rounded-lg p-1 absolute bottom-0 left-1/2 transform -translate-x-1/2 transition-opacity">
+        <div className="custom-tooltip bg-gray-800 text-white text-xs rounded-lg p-2 absolute top-full left-1/2 transform -translate-x-1/2">
+          {tooltip}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* 使用 :hover 伪类来触发自定义动画 */}
+      <style jsx>{`
+        .group:hover .w-10 {
+          animation: changeShape 0.5s ease forwards;
+        }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        @keyframes changeShape {
+          0% {
+            border-radius: 50%; /* 开始时为圆形 */
+          }
+          100% {
+            border-radius: 8px; /* 结束时为圆角方形 */
+          }
+          .group:hover .tooltip-container {
+            visibility: visible;
+            opacity: 1;
+          }
+        }
+      `}</style>
+    </a>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+
+
+export default index;
