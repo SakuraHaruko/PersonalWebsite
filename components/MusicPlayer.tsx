3939048bd61b397c1265527e2c26fb5f90a71bdf
buguwu <buguwu.official@outlook.com>
2023-11-18 19:33:15 +0800

    update 1.0.3

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,83 +1,55 @@
-import React, { useRef, useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+// MusicPlayer.tsx
 
-interface AudioPlayerProps {
-  src: string;
+import React, { Component } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+
+interface MusicPlayerProps {
+  playlist: { name: string; src: string }[];
 }
 
-const AudioPlayer: React.FC<AudioPlayerProps> = ({ src }) => {
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [progress, setProgress] = useState(0);
+interface PlayListState {
+  currentMusicIndex: number;
+}
 
-  const handleProgressChange = (e: React.MouseEvent<HTMLDivElement>) => {
-    const progressBar = e.currentTarget;
-    const clickPositionX = e.nativeEvent.offsetX;
-    const progressBarWidth = progressBar.offsetWidth;
-    const newProgress = (clickPositionX / progressBarWidth) * 100;
-
-    if (audioRef.current) {
-      const newTime = (newProgress / 100) * audioRef.current.duration;
-      audioRef.current.currentTime = newTime;
-      setProgress(newProgress);
-    }
+class MusicPlayer extends Component<MusicPlayerProps, PlayListState> {
+  state = {
+    currentMusicIndex: 0,
   };
 
-  const togglePlay = () => {
-    if (audioRef.current) {
-      if (audioRef.current.paused) {
-        audioRef.current.play().catch((error) => {
-          console.error("Failed to play audio:", error);
-        });
-        setIsPlaying(true);
-      } else {
-        audioRef.current.pause();
-        setIsPlaying(false);
-      }
-    }
+  handleClickPrevious = (): void => {
+    this.setState((prevState) => ({
+      currentMusicIndex: prevState.currentMusicIndex === 0 ? this.props.playlist.length - 1 : prevState.currentMusicIndex - 1,
+    }));
   };
 
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.addEventListener("timeupdate", () => {
-        if (audioRef.current) {
-          const currentTime = audioRef.current.currentTime;
-        const duration = audioRef.current.duration;
-        const newProgress = (currentTime / duration) * 100;
-        setProgress(newProgress);
-        }
-      });
-    }
+  handleClickNext = (): void => {
+    this.setState((prevState) => ({
+      currentMusicIndex: prevState.currentMusicIndex < this.props.playlist.length - 1 ? prevState.currentMusicIndex + 1 : 0,
+    }));
+  };
 
-    return () => {
-      if (audioRef.current) {
-        audioRef.current.removeEventListener("timeupdate", () => {});
-      }
-    };
-  }, []);
+  render(): React.ReactNode {
+    const { currentMusicIndex } = this.state;
+    const { playlist } = this.props;
 
-  return (
-    <div className="bg-white rounded-lg shadow-md p-6 opacity-90 absolute left-4 bottom-4">
-      <audio src={src} preload="auto" ref={audioRef}></audio>
-      <button
-        aria-label="Play/Pause"
-        className="bg-white hover:opacity-50 shadow-lg rounded-full p-2 w-10 h-10 focus:outline-none transition-all duration-200 ease-linear"
-        type="button"
-        onClick={togglePlay}
-      >
-        {isPlaying ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
-      </button>
-
-      <div
-        className="relative w-full h-2 mt-4 bg-gray-300 cursor-pointer"
-        onClick={handleProgressChange}
-      >
-        <div className="absolute h-2 bg-blue-500" style={{ width: `${progress}%` }}></div>
-        <div className="absolute h-6 w-6 bg-blue-500 rounded-full" style={{ left: `${progress - 3}%` }}></div>
+    return (
+      <div className='bg-white justify-center rounded-lg p-4 pt-2 left-4 bottom-4 mt-4'>
+        <p className='text-pink-200 text-center'>Now Playing - {playlist[currentMusicIndex].name}</p>
+        <AudioPlayer hasDefaultKeyBindings={true} 
+          showJumpControls={false} 
+          showSkipControls={true} 
+          volume={0.5} 
+          autoPlay={true} 
+          autoPlayAfterSrcChange={true} 
+          customAdditionalControls={[]}
+          src={playlist[currentMusicIndex].src}
+          onEnded={this.handleClickNext}
+          onClickPrevious={this.handleClickPrevious}
+          onClickNext={this.handleClickNext}
+          />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default AudioPlayer;
+export default MusicPlayer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+
 import { motion } from 'framer-motion';
+import Typewriter from 'typewriter-effect';
+
 import { faTwitter, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-import SocialMediaButton from '../components/SocialMediaButton';
+import SocialMediaButton from '@/components/SocialMediaButton';
+import MusicPlayer from '@/components/MusicPlayer'; 
 
+const Index = () => {
+  //const [typewriterVisible, setTypewriterVisible] = useState(false);
 
-const index = () => {
   const container = {
     hidden: { opacity: 1, scale: 0 },
     visible: {
@@ -28,36 +33,64 @@ const index = () => {
     }
   };
 
+  /*useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setTypewriterVisible(true);
+    }, 1000); // 设置延迟时间，单位是毫秒
+  
+    return () => clearTimeout(timeoutId); // 清除定时器以防止组件卸载时内存泄漏
+  }, []); // 空数组表示只在组件挂载时运行一次*/
+
   return (
     <div>
-      <motion.div variants={container} initial="hidden" animate="visible" className="container bg-white rounded-lg shadow-md p-8 opacity-90">
-          <div className="flex items-center gap-4 border-b border-pink-200 pb-4 md:flex-nowrap flex-wrap">
-            
+      <motion.div variants={container} initial="hidden" animate="visible" className="bg-white rounded-lg shadow-md p-8 max-w-lg bg-opacity-90">
+          <div className="flex items-center bg-white rounded-lg gap-4 p-4 md:flex-nowrap flex-wrap">
           <motion.div className="item" variants={item}>
             <Image src="/avatar.jpg" alt="Avatar" width={96} height={96} className="rounded-full"/>
           </motion.div>
+
             <div>
-              <motion.div variants={item} className="item flex items-center gap-2">
-                <p className="text-xl font-semibold">Hey,I'm buguwu</p>
+              <motion.div variants={item} className="flex items-center gap-2">
+                <p className="text-xl font-semibold">Hey, I&apos;m buguwu</p>
                   <div className="flex flex-row gap-1.5">
                 </div>
               </motion.div>
-              <motion.p variants={item} className="item text-sm text-gray-300 ">TODO</motion.p>
+              <motion.p variants={item} className="text-sm text-gray-300 ">I&apos;m a 15 years old student and program developer from China</motion.p>
             </div>
         </div>
-          {/* Social Media Buttons */}
-          <motion.div variants={item} className="item flex justify-center space-x-4 mt-4">
-            <SocialMediaButton icon={faTwitter} tooltip="Twitter" link="https://x.com/buguwuofficial" />
-            <SocialMediaButton icon={faGithub} tooltip="GitHub" link="https://github.com/buguwu" />
-            <SocialMediaButton icon={faDiscord} tooltip="Discord" link="https://discordapp.com/users/762850819249930251" />
-            <SocialMediaButton icon={faEnvelope} tooltip="Email" link="mailto:i@qwq.lgbt" />
+        <motion.div variants={item} className='justify-center rounded-lg border-2 border-pink-200 duration-300 p-4 mt-4 text-pink-200 text-md font-sans bg-white'>
+            {/*{typewriterVisible && (*/}
+            <Typewriter options={{
+                                        delay: 50,
+                                        strings: ["I'm a 15 years old student and program developer from China, I like to make some interesting things, such as websites, applications, robots, etc... (!TODO)"],
+                                        loop: false,
+                                        autoStart: true,
+                                        deleteSpeed: 604800000,
+                                    }} />{/*})}*/}
           </motion.div>
+        {/*<div className="bg-white justify-center rounded-lg p-4 left-4 bottom-4 mt-4"></div>*/}
+        <motion.div variants={item}>
+        <MusicPlayer playlist={[
+            { name: 'MIMI - 心を刺す言葉だけ (feat. 初音ミク & 可不)', src: './api/music/2091770490' },
+            { name: '程嘉敏 - 小年兽', src: './api/music/1346758452' },
+            { name: '暗杠/寅子 - 说书人', src: './api/music/1198010' },
+          ]}
+          />
+        <div className='border-b border-pink-200 mt-6' />
+        </motion.div>
+        {/* Social Media Buttons */}
+        <div className='flex justify-center space-x-4 mt-4'>
+          <SocialMediaButton icon={faTwitter} tooltip="Twitter" link="https://x.com/buguwuofficial" delay={1.5} />
+          <SocialMediaButton icon={faGithub} tooltip="GitHub" link="https://github.com/buguwu" delay={2.0}/>
+          <SocialMediaButton icon={faDiscord} tooltip="Discord" link="https://discordapp.com/users/762850819249930251" delay={2.5}/>
+          <SocialMediaButton icon={faEnvelope} tooltip="Email" link="mailto:i@qwq.lgbt" delay={3.0}/>
+        </div>
       </motion.div>
-      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 17 }} className="text-gray-500 text-sm mt-4 absolute inset-x-0 bottom-0 text-center py-4">
+      <div className="text-gray-500 text-sm mt-4 absolute inset-x-0 bottom-0 text-center py-4">
         <a href="https://icp.gov.moe/?keyword=20236250" target="_blank">萌ICP备20236250号</a>
-      </motion.div>
+      </div>
     </div>
     );
   };
   
-export default index;
+export default Index;
