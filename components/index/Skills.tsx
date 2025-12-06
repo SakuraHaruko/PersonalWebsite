bdb87cb4208845c30784d01728425498df852d01
SakuraHaruko <buguwu.official@outlook.com>
2025-12-06 18:46:51 +0800

    update nextjs to 16.0.7

--- components/index/Skills.tsx
+++ components/index/Skills.tsx
@@ -2,8 +2,8 @@
 
 import TypeScriptIcon from "@/public/techIcons/TypeScript.svg";
 import SpringBootIcon from "@/public/techIcons/SpringBoot.svg";
-import MicrosoftNETIcon from  "@/public/techIcons/Microsoft.NET.svg";
-import KotlinIcon from  "@/public/techIcons/Kotlin.svg";
+import MicrosoftNETIcon from "@/public/techIcons/Microsoft.NET.svg";
+import KotlinIcon from "@/public/techIcons/Kotlin.svg";
 import PythonIcon from "@/public/techIcons/Python.svg";
 import JavaIcon from "@/public/techIcons/Java.svg";
 import ReactIcon from "@/public/techIcons/React.svg";
@@ -26,6 +26,7 @@ export default function MySkills() {
                         watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">My Skills</h2>
                     </TranslateInOut>
+
                     <FadeInOut
                         durationIn={0.5}
                         delay={1.1}
@@ -36,105 +37,83 @@ export default function MySkills() {
                         </p>
                     </FadeInOut>
                 </div>
+
                 <div className="mx-auto grid max-w-5xl items-center gap-6 sm:grid-cols-2 md:gap-12 lg:grid-cols-4 lg:gap-24">
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={1.0}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* React */}
+                    <TranslateInOut durationIn={1.0} delay={1.0} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <ReactIcon />
+                            <Image src={ReactIcon} alt="React" width={48} height={48} draggable={false} />
                             <span className="font-medium">React</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.1}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* Next.js */}
+                    <TranslateInOut durationIn={1.0} delay={2.1} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <Image src="/techIcons/NextJS.png" alt="Next.js" width="48" height="48" draggable={false} />
+                            <Image src="/techIcons/NextJS.png" alt="Next.js" width={48} height={48} draggable={false} />
                             <span className="font-medium">Next.js</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.2}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* TypeScript */}
+                    <TranslateInOut durationIn={1.0} delay={2.2} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <TypeScriptIcon />
+                            <Image src={TypeScriptIcon} alt="TypeScript" width={48} height={48} draggable={false} />
                             <span className="font-medium">TypeScript</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.3}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* Kotlin */}
+                    <TranslateInOut durationIn={1.0} delay={2.3} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <KotlinIcon />
+                            <Image src={KotlinIcon} alt="Kotlin" width={48} height={48} draggable={false} />
                             <span className="font-medium">Kotlin</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.4}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* Java */}
+                    <TranslateInOut durationIn={1.0} delay={2.4} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <JavaIcon />
+                            <Image src={JavaIcon} alt="Java" width={48} height={48} draggable={false} />
                             <span className="font-medium">Java</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.5}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* Spring Boot */}
+                    <TranslateInOut durationIn={1.0} delay={2.5} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <SpringBootIcon />
+                            <Image src={SpringBootIcon} alt="SpringBoot" width={48} height={48} draggable={false} />
                             <span className="font-medium">SpringBoot</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.6}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* .NET */}
+                    <TranslateInOut durationIn={1.0} delay={2.6} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <MicrosoftNETIcon />
+                            <Image src={MicrosoftNETIcon} alt=".NET" width={48} height={48} draggable={false} />
                             <span className="font-medium">Microsoft .NET</span>
                         </div>
                     </TranslateInOut>
-                    <TranslateInOut
-                        durationIn={1.0}
-                        delay={2.7}
-                        y="100%"
-                        start="-100% bottom"
-                        end="top top"
-                        watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
+
+                    {/* Python */}
+                    <TranslateInOut durationIn={1.0} delay={2.7} y="100%" start="-100% bottom" end="top top"
+                                    watch overflowHidden={undefined} skipOutro={undefined} markers={undefined}>
                         <div className="flex flex-col items-center space-y-2">
-                            <PythonIcon />
+                            <Image src={PythonIcon} alt="Python" width={48} height={48} draggable={false} />
                             <span className="font-medium">Python</span>
                         </div>
                     </TranslateInOut>
+
                 </div>
             </div>
         </section>
-    )
+    );
 }
